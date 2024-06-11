ce06a50077413af2e29e65639d6492ac22dcea52
Benjamin Leigh-Gladeau <000785704@mohawkcollege.ca>
2024-06-11 10:29:39 -0400

    setup

---
+++ Assignment 3 Code/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_3_Code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
